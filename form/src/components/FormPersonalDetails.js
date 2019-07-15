68e199a555e59db4d053202b51608ed0feb32b47
Imon Ovbude <imonovbude@gmail.com>
2019-07-15 14:02:29 -0700

    created userform.js file

---
+++ form/src/components/FormPersonalDetails.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class FormPersonalDetails extends React.Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default FormPersonalDetails;
---
+++ form/src/components/Success.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Success extends React.Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default Success;
