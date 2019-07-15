f1ce1b98a2aace98d0a77c2bf25820fd8d4c03d8
Imon Ovbude <imonovbude@gmail.com>
2019-07-15 15:09:57 -0700

    completed succes component

--- form/src/components/Success.js
+++ form/src/components/Success.js
@@ -1,9 +1,17 @@
 import React from "react";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import AppBar from "material-ui/AppBar";
 
-class Success extends React.Component {
-  render() {
-    return <div />;
-  }
-}
+const Success = () => {
+  return (
+    <MuiThemeProvider>
+      <>
+        <AppBar title="Success" />
+        <h1>Thank You For Your Submition</h1>
+        <p>You Will Recieve and Email With Further Instructions</p>
+      </>
+    </MuiThemeProvider>
+  );
+};
 
 export default Success;
